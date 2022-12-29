4f9a2f1ec99aa719a4745b86f07180967ca210a0
Arda Balkan <119343751+engardabalkan@users.noreply.github.com>
2022-12-29 13:46:44 +0300

    Add files via upload

---
+++ SalaryCalculator/Main.java
@@ -0,0 +1,8 @@
+package SalaryCalculator;
+
+public class Main {
+        public static void main(String[] args) {
+                Employee employee = new Employee("David",10000,45,2000);
+                employee.printEmployee();
+        }
+}
